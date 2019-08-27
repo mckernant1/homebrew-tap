6f3f63a43887a81e162fd5a9fe61d4531b195665
Tom McKernan <tmeaglei@gmail.com>
2019-08-27 11:39:22 -0400

    gg 0.1.1

--- global-git.rb
+++ global-git.rb
@@ -1,8 +1,8 @@
 class GlobalGit < Formula
   desc "A global git repo manager"
   homepage "https://github.com/mckernant1/global_git"
-  url "https://github.com/mckernant1/global_git/archive/0.0.1.tar.gz"
-  sha256 "1fdcc6dd518495b53180093c47cec48cda3e80894ceec741c6c3808a4360ed88"
+  url "https://github.com/mckernant1/global_git/archive/0.1.1.tar.gz"
+  sha256 "70b48eac9cd9c237d3ca27f4187d600f21f060a544ecad4a24ccdfc9b9da7792"
 
   depends_on "rust" => :build
   depends_on "git"
