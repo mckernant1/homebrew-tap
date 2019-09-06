d01a8cad06d2908dd587bf12d9cf1600c8320b94
Tom McKernan <tmeaglei@gmail.com>
2019-09-06 13:48:22 -0400

    pm 0.3.1

--- project-manager.rb
+++ project-manager.rb
@@ -1,8 +1,12 @@
 class ProjectManager < Formula
   desc "Your personal project manager"
   homepage "https://github.com/mckernant1/project_manager"
-  url "https://github.com/mckernant1/project_manager/archive/0.3.0.tar.gz"
-  sha256 "0ab4bd769eca29e7a92fc5a3e8108cb7ccdbf31202e01ee92b21f99b3dd7ab60"
+  url "https://github.com/mckernant1/project_manager/archive/0.3.1.tar.gz"
+  sha256 "2313adcdb5632e43e5844d8df652b462368b378920c3bfe0fc7b3a647ead0748"
+
+  bottle do
+    sha256 "67fef59bf562dcc1373cc9eb006d0ad13adbc5ae7ec399b82637a68e61683260" => :mojave
+  end
 
   depends_on "rust" => :build
   depends_on "git"
