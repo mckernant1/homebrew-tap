b575b2fd11ced22b11c7939238ed0f2e68f851b1
Tom McKernan <tmeaglei@gmail.com>
2019-09-06 13:57:58 -0400

    removing bottles

--- project-manager.rb
+++ project-manager.rb
@@ -4,12 +4,6 @@ class ProjectManager < Formula
   url "https://github.com/mckernant1/project_manager/archive/0.3.1.tar.gz"
   sha256 "2313adcdb5632e43e5844d8df652b462368b378920c3bfe0fc7b3a647ead0748"
 
-  bottle do
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "3f49d8f6bc1b3b5b8513ad1659c3cbb4cb8f00bf3fc0d32e2222f60cc8cfa0a2" => :mojave
-  end
-
   depends_on "rust" => :build
   depends_on "git"
 
