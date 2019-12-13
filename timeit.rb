e7d8863596872fb509b3725dd33e74c21c500221
Tom McKernan <tmeaglei@gmail.com>
2019-12-13 12:25:20 -0500

    timeit 0.2.0

--- timeit.rb
+++ timeit.rb
@@ -1,8 +1,8 @@
 class Timeit < Formula
   desc "A timer that times the next command"
   homepage "https://github.com/mckernant1/timeit"
-  url "https://github.com/mckernant1/timeit/archive/0.1.0.tar.gz"
-  sha256 "8480a69627d49fe877d21d07670f340ad894385e9f64ec25a0c9906e37f571f5"
+  url "https://github.com/mckernant1/timeit/archive/0.2.0.tar.gz"
+  sha256 "80c3ec48dffe4cf1115520de0437495eec9291ccb65d2d82744aac0f85c29e8b"
 
   depends_on "rust" => :build
 
