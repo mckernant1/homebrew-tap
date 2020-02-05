dd09e1ce304e51db4fc4aa430aa31e982e4fa39f
Tom McKernan <tmeaglei@gmail.com>
2020-02-05 15:53:15 -0800

    0.1.2

--- auto-updater.rb
+++ auto-updater.rb
@@ -1,8 +1,8 @@
 class AutoUpdater < Formula
   desc "Automatically updates added package managers"
   homepage "https://github.com/mckernant1/auto-updater"
-  url "https://github.com/mckernant1/auto-updater/archive/0.1.1.tar.gz"
-  sha256 "9d3956d70b099d090ad4023543c9ce7ae680746a8dd96e146a727dadbad3bdca"
+  url "https://github.com/mckernant1/auto-updater/archive/0.1.2.tar.gz"
+  sha256 "bad4d5b28b2fe69a9795026b587ffeca50a821d691e7622311ba726b94c5c3dd"
 
   depends_on "rust" => :build
   depends_on "git"
