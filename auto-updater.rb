2679472d7c40e34f2829921e3a15be8475f39eee
Tom McKernan <tmeaglei@gmail.com>
2020-02-05 15:43:48 -0800

    0.1.1

--- auto-updater.rb
+++ auto-updater.rb
@@ -1,14 +1,25 @@
 class AutoUpdater < Formula
   desc "Automatically updates added package managers"
   homepage "https://github.com/mckernant1/auto-updater"
-  url "https://github.com/mckernant1/auto-updater/archive/0.1.0.tar.gz"
-  sha256 "60cfa0bb425d48d70f15801fa2686701509a239c7396ca44d336317e796fc261"
+  url "https://github.com/mckernant1/auto-updater/archive/0.1.1.tar.gz"
+  sha256 "9d3956d70b099d090ad4023543c9ce7ae680746a8dd96e146a727dadbad3bdca"
 
   depends_on "rust" => :build
   depends_on "git"
 
   def install
     system "cargo", "install", "--root", prefix, "--path", "."
+  end
+
+  def caveats; <<~EOS
+    To get this to run automatically add to your zshrc or bashrc
+
+    auto-updater upgrade
+
+    You can also run manually with
+
+    auto-updater upgrade [-f] [NAME]
+  EOS
   end
 
 end
