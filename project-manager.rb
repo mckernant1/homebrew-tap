99ce52792b1721e10785b522f6a86a6fe89fe46c
Tom McKernan <tmeaglei@gmail.com>
2020-02-19 17:35:35 -0800

    fixing hash

--- project-manager.rb
+++ project-manager.rb
@@ -2,7 +2,7 @@ class ProjectManager < Formula
   desc "Your personal project manager"
   homepage "https://github.com/mckernant1/project-manager"
   url "https://github.com/mckernant1/project_manager/archive/0.3.3.tar.gz"
-  sha256 "d76334749c58ce1f7d35c418bdda0d1a2a5740f62bb3e8b95c4887b9b91dd9b5"
+  sha256 "77809601f1c30f5e5f51c112ab8e07de55a3122fe8cfb9a49c1ec8457dbee5f1"
 
   depends_on "rust" => :build
   depends_on "git"
