25e05713f097a1a48a1c1b8d75ae3f7bf68d572f
Tom McKernan <tmeaglei@gmail.com>
2020-12-06 15:30:21 -0800

    swain update

--- Formula/swain.rb
+++ Formula/swain.rb
@@ -1,21 +1,15 @@
-class Hasher < Formula
-  desc "A hash verifier"
-  homepage "https://github.com/mckernant1/hasher"
-  url "https://github.com/mckernant1/hasher/archive/0.4.tar.gz"
-  sha256 "279468bdc607b5eb9903f0f8fda586e13b47232f78af509634c6cd37474314f7"
+class Swain < Formula
+  desc ""
+  homepage "https://github.com/mckernant1/swain"
+  url "https://github.com/mckernant1/swain/archive/0.0.1.tar.gz"
+  sha256 "08013e379dab9eec5f6f1fb7e3affa3f03f8bc643bc1853c590052ad33d519f6"
 
   depends_on :java => "1.8"
 
-  bottle do
-    root_url "https://homebrew.bintray.com/bottles-tools"
-    cellar :any_skip_relocation
-    sha256 "3aca9f1843fbed13c11c2bc5a754c29146b4ef2e68216276a539730c1b0e9a9e" => :catalina
-  end
-
   def install
     system "./gradlew", "shadowJar"
-    libexec.install "build/libs/hasher-all.jar"
-    bin.write_jar_script libexec/"hasher-all.jar", "hasher", :java_version => "1.8"
+    libexec.install "build/libs/swain-1.0-SNAPSHOT-all.jar"
+    bin.write_jar_script libexec/"swain-1.0-SNAPSHOT-all.jar", "swain", :java_version => "1.8"
   end
 
 end
