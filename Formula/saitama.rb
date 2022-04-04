06e7950c7bea4cf7384ba34ed6021419c7f17f53
Thomas McKernan <tmeaglei@gmail.com>
2022-04-03 19:47:13 -0700

    saitama 0.0.2

--- Formula/saitama.rb
+++ Formula/saitama.rb
@@ -1,16 +1,16 @@
 class Saitama < Formula
   desc "Load Testing CLI"
   homepage "https://github.com/mckernant1/saitama"
-  url "https://github.com/mckernant1/saitama/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "9a536dd544b0d68a01cd76b7e48d1604f0a2f2a92e8d3a70d072f8da3d9395cc"
+  url "https://github.com/mckernant1/saitama/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "af9ab03136ea750a56215973d01bc04b66a6cd94a55a268e18c79eff6cff73c8"
 
   depends_on "rust" => :build
   depends_on "git"
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
-    # bash_completion.install "completions/saitama.bash"
-    # zsh_completion.install "completions/_saitama"
+    bash_completion.install "completions/saitama.bash"
+    zsh_completion.install "completions/_saitama"
   end
 
 end
