f48b2b822ad9818d593cc53f757da95a4f9aa053
Thomas McKernan <tmeaglei@gmail.com>
2022-06-04 12:25:55 -0700

    speedbag 0.0.3

--- Formula/speedbag.rb
+++ Formula/speedbag.rb
@@ -1,8 +1,8 @@
 class Speedbag < Formula
   desc "Sample Server"
   homepage "https://github.com/mckernant1/speedbag"
-  url "https://github.com/mckernant1/speedbag/archive/refs/tags/0.0.2.tar.gz"
-  sha256 "3a6dd751e7e50b48c11ecb066bf160af6f53bc1584984cbd2f9a79255014ad25"
+  url "https://github.com/mckernant1/speedbag/archive/refs/tags/0.0.3.tar.gz"
+  sha256 "fb692452d5963feaddd5dfac94e6f4fa225e29be33253476e9eb9cdeacf0e56a"
 
   depends_on "rust" => :build
   depends_on "git"
