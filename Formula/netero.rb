c810b15cae938ead2c4b4208eddd7a423f9a0aa7
Thomas McKernan <tmeaglei@gmail.com>
2022-11-13 14:12:46 -0800

    netero 0.0.9

--- Formula/netero.rb
+++ Formula/netero.rb
@@ -1,8 +1,8 @@
 class Netero < Formula
   desc "Load Testing CLI"
   homepage "https://github.com/mckernant1/netero"
-  url "https://github.com/mckernant1/netero/archive/refs/tags/0.0.8.tar.gz"
-  sha256 "38f01cd76d1885bd5ff7bdca23247dced16f875bc20d6f687a7de690492dd285"
+  url "https://github.com/mckernant1/netero/archive/refs/tags/0.0.9.tar.gz"
+  sha256 "66c2a66e2a75d1168ace9311a8a666cf436ec7074a3eb6b7a4995c027c50716f"
 
   depends_on "rust" => :build
   depends_on "git"
